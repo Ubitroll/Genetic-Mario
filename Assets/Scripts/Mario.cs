7c43e1e0f62b2fa891d75e53d15b59e93c59570c
jokehboy <jokehboy@gmail.com>
2021-03-17 15:58:51 +0000

    funny movement done

--- Assets/Scripts/Mario.cs
+++ Assets/Scripts/Mario.cs
@@ -4,15 +4,117 @@ using UnityEngine;
 
 public class Mario : MonoBehaviour
 {
+    [SerializeField] private LayerMask platformLayerMask;
+    public Vector2 position;
+    public Vector2 velocity;
+    public float jumpVel;
+
+    public float speed;
+    public float frictionValue;
+    public float opposingForce;
+
+    public BoxCollider2D boxCollider;
+
+    public float currentSpeed;
+
+    public bool jumpPressed = false;
+    public bool jumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, platformLayerMask);
+        Color rayColor;
+
+        if(raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+
+        Debug.DrawRay(boxCollider.bounds.center + new Vector3(boxCollider.bounds.extents.x, 0f), Vector2.down * (boxCollider.bounds.extents.y + 0.1f), rayColor);
+        Debug.DrawRay(boxCollider.bounds.center - new Vector3(boxCollider.bounds.extents.x, 0f), Vector2.down * (boxCollider.bounds.extents.y + 0.1f), rayColor);
+        Debug.DrawRay(boxCollider.bounds.center - new Vector3(0f,boxCollider.bounds.extents.y), Vector2.right * (boxCollider.bounds.extents.y + 0.1f), rayColor);
+        return raycastHit.collider != null;
+    }
+
+    public bool TouchingWallLeft()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.left, 0.2f, platformLayerMask);
+        Color rayColor;
+
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+
+        Debug.DrawRay(boxCollider.bounds.center + new Vector3(boxCollider.bounds.extents.y, 0f), Vector2.left * (boxCollider.bounds.extents.y + 0.1f), rayColor);
+        
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
+    public bool TouchingWallRight()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.right, 0.2f, platformLayerMask);
+        Color rayColor;
+
+        if (raycastHit.collider != null)
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+
+        Debug.DrawRay(boxCollider.bounds.center + new Vector3(boxCollider.bounds.extents.y, 0f), Vector2.left * (boxCollider.bounds.extents.y + 0.1f), rayColor);
+
+        Debug.Log(raycastHit.collider);
+        return raycastHit.collider != null;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        jumpPressed = Input.GetButtonDown("Jump");
+
+
+        if (IsGrounded() && jumpPressed )
+        {
+          
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(this.GetComponent<Rigidbody2D>().velocity.x, jumpVel);
+        }
+        if( Input.GetAxisRaw("Horizontal") > 0 && !TouchingWallRight())
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, this.GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0 && !TouchingWallLeft())
+        {
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, this.GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 0 )
+        {
+            currentSpeed = this.GetComponent<Rigidbody2D>().velocity.x;
+            opposingForce = -currentSpeed;
+
+            this.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(opposingForce * frictionValue, 0));
+        }
+
+
+
+
     }
 }
